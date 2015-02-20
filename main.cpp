09710598b8393b0c3f246ab0be722e2767da88e9
Francisco Maria Calisto <francisco_mcalisto@hotmail.com>
2015-02-20 22:11:45 +0000

    Update
    Update file.

--- main.cpp
+++ main.cpp
@@ -16,22 +16,40 @@
     arabiannights::Genie *genies[5];                  // vector for genies
  
     // 2. Esfregar 5 vezes a lâmpada, indicando os números de desejos 2, 3, 4, 5, 1.
-    for (size_t gx = 0; gx < 5; gx++) genies[gx] = ml.rub(wishes[gx]);
+    for (size_t gx = 0; gx < 5; gx++)
+    {
+    	genies[gx] = ml.rub(wishes[gx]);
+    }
  
     // 3. Imprimir em std::cout (utilizando o operador <<) cada um dos génios.
-    for (size_t gx = 0; gx < 5; gx++) std::cout << *genies[gx] << std::endl;
+    for (size_t gx = 0; gx < 5; gx++)
+    {
+    	std::cout << *genies[gx] << std::endl;
+    }
  
     // 4. Pedir um desejo a cada um dos génios.
-    for (size_t gx = 0; gx < 5; gx++) genies[gx]->grantWish();
+    for (size_t gx = 0; gx < 5; gx++)
+    {
+    	genies[gx]->grantWish();
+    }
  
     // 5. Imprimir em std::cout (utilizando o operador <<) cada um dos génios.
-    for (size_t gx = 0; gx < 5; gx++) std::cout << *genies[gx] << std::endl;
+    for (size_t gx = 0; gx < 5; gx++)
+    {
+    	std::cout << *genies[gx] << std::endl;
+    }
  
     // 6. Pedir um desejo a cada um dos génios.
-    for (size_t gx = 0; gx < 5; gx++) genies[gx]->grantWish();
+    for (size_t gx = 0; gx < 5; gx++)
+    {
+    	genies[gx]->grantWish();
+    }
  
     // 7. Imprimir em std::cout (utilizando o operador <<) cada um dos génios.
-    for (size_t gx = 0; gx < 5; gx++) std::cout << *genies[gx] << std::endl;
+    for (size_t gx = 0; gx < 5; gx++)
+    {
+    	std::cout << *genies[gx] << std::endl;
+    }
  
     // 8. Colocar o demónio reciclável na lâmpada.
     // tricky (how can we be certain the demon is in the 5th position?)
